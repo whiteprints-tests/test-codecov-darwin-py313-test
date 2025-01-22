075ea00db775b1bd4ea402b44ab4b3a01be80daf
Romain Brault <mail@romainbrault.com>
2025-01-22 03:23:47 +0000

    feat: ✨ add command line interface

---
+++ docs/usage.md
@@ -0,0 +1,22 @@
+<!--
+SPDX-FileCopyrightText: © 2025 Romain Brault <mail@romainbrault.com>
+
+SPDX-License-Identifier: CC0-1.0
+-->
+
+# 💻 Usage
+
+```{eval-rst}
+
+.. click:: test_codecov_darwin_py313_test.cli.entrypoint:test_codecov_darwin_py313_test
+    :prog: whiteprint
+    :nested: full
+```
+
+```{toctree}
+---
+hidden:
+---
+
+Usage <self>
+```
---
+++ src/test_codecov_darwin_py313_test/cli/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: © 2025 Romain Brault <mail@romainbrault.com>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+"""Everything related to the command line interface."""
+
+from typing import Final
+
+
+__all__: Final = ["__app_name__"]
+
+__app_name__: Final = "test-codecov-darwin-py313-test"
+"""The name of the application."""
